30763396f21c8b8483e8d6dfff194e3da7cd5a17
Yash Kumar Verma <healthedprohacathon@gmail.com>
2025-03-15 21:42:47 +0530

    Update script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,7 +9,7 @@ let userMessage = null;
 let isResponseGenerating = false;
 
 // API configuration
-const API_KEY = "AIzaSyCaVXuHzLSBtxlY1WXZbb5CZjnVosuEV08"; // Your API key here
+const API_KEY = "AIzaSyAY0vCu4VmDGZ-JpHZ0s3D7H46LQSVqlO8"; // Your API key here
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
 
 // Load theme and chat data from local storage on page load
